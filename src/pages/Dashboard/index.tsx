e0724dddf1a1dc0d2c079c9afe7a6eb594953c2e
pablohariel <pablohhdr@gmail.com>
2021-05-27 00:36:52 -0300

    API connection

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,69 +1,70 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import { api } from '../../services/api';
 
 import logoImg from '../../assets/github-logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no Github</Title>
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
 
-    <Form action="">
-      <input placeholder="Digite o nome do repositorio" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
-    <Repositories>
-      <a href="test">
-        <img src="https://avatars.githubusercontent.com/u/52895805?v=4" alt="Pablo Hariel" />
-        <div>
-          <strong>rocketseat/unform</strong>
-          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-        </div>
+  const handleAddRepository = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault();
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="test">
-        <img src="https://avatars.githubusercontent.com/u/52895805?v=4" alt="Pablo Hariel" />
-        <div>
-          <strong>rocketseat/unform</strong>
-          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-        </div>
+      const { full_name, description, owner } = response.data;
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="test">
-        <img src="https://avatars.githubusercontent.com/u/52895805?v=4" alt="Pablo Hariel" />
-        <div>
-          <strong>rocketseat/unform</strong>
-          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-        </div>
+      const repository = {
+        full_name,
+        description,
+        owner,
+      };
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="test">
-        <img src="https://avatars.githubusercontent.com/u/52895805?v=4" alt="Pablo Hariel" />
-        <div>
-          <strong>rocketseat/unform</strong>
-          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-        </div>
+      setRepositories([repository, ...repositories]);
+      setNewRepo('');
+    } catch (err) {
+      console.log('error');
+    }
+  };
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="test">
-        <img src="https://avatars.githubusercontent.com/u/52895805?v=4" alt="Pablo Hariel" />
-        <div>
-          <strong>rocketseat/unform</strong>
-          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-        </div>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
 
-        <FiChevronRight size={20} />
-      </a>
+      <Form onSubmit={handleAddRepository}>
+        <input value={newRepo} onChange={(e) => setNewRepo(e.target.value)} placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-    </Repositories>
-  </>
-);
+      <Repositories>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href={repository.full_name}>
+            <img src={repository.owner.avatar_url} alt="Pablo Hariel" />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description ? repository.description : 'No description'}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export { Dashboard };
